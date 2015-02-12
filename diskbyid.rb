1cfc95dda27e4fd3de670c866df57523060b8ace
Danny van der Meulen <dmeulen@ebay.com>
2015-02-13 00:57:08 +0100

    skip if in exclude list

--- diskbyid.rb
+++ diskbyid.rb
@@ -10,6 +10,7 @@ if Facter.value(:kernel) == 'Linux'
   Facter::Util::Resolution.exec('find /dev/disk/by-id -type l -printf "%f %l\n" 2> /dev/null').each_line do |line|
     line.strip!
     next if line.empty?
+    next if line.match(exclude)
     id   = line.split(/\s+/)[0]
     disk = line.split(/\s+/)[1]
     disks[disk] = id
