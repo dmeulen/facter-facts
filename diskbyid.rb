0b9b1e5b1a8d53f46c48f5070d161682f523437d
Danny van der Meulen <dmeulen@ebay.com>
2015-02-13 00:54:34 +0100

    added disk by id fact

---
+++ diskbyid.rb
@@ -0,0 +1,23 @@
+#
+# diskbyid.rb
+if Facter.value(:kernel) == 'Linux'
+
+  disks = Hash.new { |k,v| k[v] = [] }
+
+  exclude = %w(backdev.* dm-\d loop md mmcblk mtdblock ramzswap)
+  exclude = Regexp.union(*exclude.collect { |i| Regexp.new(i) })
+
+  Facter::Util::Resolution.exec('find /dev/disk/by-id -type l -printf "%f %l\n" 2> /dev/null').each_line do |line|
+    line.strip!
+    next if line.empty?
+    id   = line.split(/\s+/)[0]
+    disk = line.split(/\s+/)[1]
+    disks[disk] = id
+  end
+
+  disks.each do |k,v|
+    Facter.add("disk_#{k.tr('./','')}") do
+      setcode { v }
+    end
+  end
+end
